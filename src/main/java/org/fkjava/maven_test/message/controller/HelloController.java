92468a9e47937b6b6e9609ecc89d9b26a6e375ec
lhy_s <lhy_s@163.com>
2019-07-03 22:23:58 +0800

    1111

--- src/main/java/org/fkjava/maven_test/message/controller/HelloController.java
+++ src/main/java/org/fkjava/maven_test/message/controller/HelloController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
 	@RequestMapping("/hello")
 	public String hello(){
+		System.out.println("fix finish");
 		return "hello my frist spring boot myfirst";
 	}
 	
