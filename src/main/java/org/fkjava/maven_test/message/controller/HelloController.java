36bf857fe87abc98a55b2f932b7523d044946d60
lhy_s <lhy_s@163.com>
2019-07-03 16:19:20 +0800

    myfirst commit for eclipse

---
+++ src/main/java/org/fkjava/maven_test/message/controller/HelloController.java
@@ -0,0 +1,17 @@
+package org.fkjava.maven_test.message.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+/**
+ * @RestController相当于springmvc中的@Controller和@ResponseBody
+ * @author tony
+ *
+ */
+@RestController
+public class HelloController {
+	@RequestMapping("/hello")
+	public String hello(){
+		return "hello my frist spring boot";
+	}
+	
+}
